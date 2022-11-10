c4aaad7f74ff68bd30825a20a7441ae869b8cf3a
Parker Holcomb <parker.holcomb@anthem.com>
2022-11-10 06:06:59 -0600

    tidy

--- src/dqfit/model.py
+++ src/dqfit/model.py
@@ -5,6 +5,16 @@ import plotly.express as px
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from pathlib import Path
 
+
+SUPPORTED_RESOURCES = [
+    "Patient",
+    "Procedure",
+    "Condition",
+    "Observation",
+    "Immunization",
+    # "AllergyIntolerance", getting erros on synthrea
+]
+
 # could be an abstraction
 class DQIModel:
     def __init__(self, context="systolic") -> None:
@@ -15,11 +25,14 @@ class DQIModel:
     def weights(self):
         package_dir = Path(__file__).parent
         dimension_weights = pd.read_csv(
-            f"{package_dir}/weights/{self.context}/dimension_weights.csv")
+            f"{package_dir}/weights/{self.context}/dimension_weights.csv"
+        )
         feature_weights = pd.read_csv(
-            f"{package_dir}/weights/{self.context}/feature_weights.csv")
+            f"{package_dir}/weights/{self.context}/feature_weights.csv"
+        )
         weights = pd.concat([feature_weights, dimension_weights])[
-            ['feature', 'dimension', 'weight']]
+            ["feature", "dimension", "weight"]
+        ]
         return weights
 
     def fit_transform(self, bundles: Union[pd.DataFrame, list]) -> pd.DataFrame:
@@ -28,12 +41,35 @@ class DQIModel:
             bundles = pd.DataFrame(bundles)
 
         if self.context == "systolic":
-            return self._base_transform(bundles)
+            scored_bundles = self._base_transform(bundles)
         else:
-            return self._alt_transform(bundles)
+            scored_bundles = self._alt_transform(bundles)
+
+        scored_bundles = scored_bundles.sort_values(["group", "fitness_score"], ascending=False).reset_index(drop=True)
+        print(f"Context: {self.context}")
+        print(scored_bundles["group"].value_counts())
+        return scored_bundles
 
     @staticmethod
-    def _base_transform(bundles):
+    def _base_transform(bundles, tolerance=7):
+        """ "
+        MinMx, then parse to 0-100 int
+        """
+
+        bundles["y"] = bundles["entry"].apply(lambda x: len(x))
+        bundles[["y"]] = MinMaxScaler().fit_transform(bundles[["y"]])
+        bundles["fitness_score"] = bundles["y"].apply(lambda x: int(x * 100))
+        bundles["group"] = bundles["fitness_score"].apply(
+            lambda x: "pass" if x > tolerance else "fail"
+        )
+
+        bundles = bundles.sort_values(
+            ["group", "fitness_score"], ascending=False
+        ).reset_index(drop=True)
+        return bundles
+
+    @staticmethod
+    def _alt_transform(bundles):
         """ "
         StandardScaler, capped, with minmax, then parse to 0-100 int
         """
@@ -51,22 +87,12 @@ class DQIModel:
 
         bundles["y"] = bundles["y"].apply(_cap_z)
         bundles[["y"]] = MinMaxScaler().fit_transform(bundles[["y"]])
-        bundles["score"] = bundles["y"].apply(lambda x: int(x * 100))
-        bundles["group"] = bundles["score"].apply(
-            lambda x: "pass" if x > 7 else "fail")
-        return bundles
+        bundles["fitness_score"] = bundles["y"].apply(lambda x: int(x * 100))
+        bundles["group"] = bundles["fitness_score"].apply(
+            lambda x: "pass" if x > 7 else "fail"
+        )
 
-    @staticmethod
-    def _alt_transform(bundles):
-        """ "
-        Like the other one but different
-        """
-
-        bundles["y"] = bundles["entry"].apply(lambda x: len(x))
-        bundles[["y"]] = MinMaxScaler().fit_transform(bundles[["y"]])
-        bundles["score"] = bundles["y"].apply(lambda x: int(x * 100))
-        bundles["group"] = bundles["score"].apply(
-            lambda x: "pass" if x > 7 else "fail")
+        
         return bundles
 
     def visualize(self, scored_bundles: pd.DataFrame) -> None:
